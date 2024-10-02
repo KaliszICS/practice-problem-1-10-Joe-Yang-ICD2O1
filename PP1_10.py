fa552159810c5b6ddc4bb78870a0e3c394cd3b49
Joe-Yang-ICD2O1 <joe.yang@student.tdsb.on.ca>
2024-10-02 13:36:43 +0000

    answer

--- PP1_10.py
+++ PP1_10.py
@@ -1,23 +1,44 @@
+'''
+    Lesson: Math Library
+    Author: Joe Yang
+    Date Created: October 2, 2024
+    Date Last Modified: October 2, 2024
+'''
 
+  import math 
 
 def q1(): 
   #Write Assignment code here
 
+  num= float(input("Input a number: "))
+  print(math.sqrt(num))
 
 def q2(): 
   #Write Assignment code here
 
+  num= int(input("Input a number: "))
+  print(math.isqrt(num))
 
 def q3(): 
   #Write Assignment code here
+
+  num= float(input("Input a number: "))
+  print(math.floor(num))
 
 
 def q4(): 
   #Write Assignment code here
 
+  num= float(input("Input a number: "))
+  print(math.ceil(num))
 
 def q5(): 
   #Write Assignment code here
+
+  num= float(input("Input a number: "))
+  num2= float(input("Input another number: "))
+  num= (num * num2 / 2)
+  print(math.trunc(num))
 
 
 
